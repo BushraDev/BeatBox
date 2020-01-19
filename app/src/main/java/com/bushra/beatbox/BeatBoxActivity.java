330319d091b3be124ccf8a359ce5395f4b92b05b
Bushra Al-Mahdi <diamondprincessbushra@gmail.com>
2020-01-19 11:52:42 +0300

    Initial commit

---
+++ app/src/main/java/com/bushra/beatbox/BeatBoxActivity.java
@@ -0,0 +1,12 @@
+package com.bushra.beatbox;
+
+import androidx.fragment.app.Fragment;
+
+public class BeatBoxActivity extends  SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return BeatBoxFragment.newInstance();
+    }
+
+}
